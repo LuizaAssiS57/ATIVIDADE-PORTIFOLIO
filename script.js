b449bcada97b4bc8ad5da0b95bc51b749989b0a0
LuizaAssiS57 <luizaassis57@gmail.com>
2026-05-28 15:56:27 -0300

    Mudando o tema.

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Mantém o ano do rodapé atualizado automaticamente
+// Define automaticamente o ano atual no rodapé da página
 document.getElementById('ano-atual').textContent = new Date().getFullYear();
 
-// Controla a abertura e fechamento das descrições de tecnologias
+// Função para abrir e fechar a explicação de cada tecnologia
 function toggleCard(item) {
-    // Alterna a classe active no item clicado
     item.classList.toggle('active');
 }
 
-// Controla a alternância do Modo Escuro
+// Função para ativar/desativar o Modo Escuro
 function toggleDarkMode() {
-    // Liga/Desliga a classe 'dark-mode' no elemento <body>
     document.body.classList.toggle('dark-mode');
     
-    // Altera o texto do botão para combinar com o modo atual
     const botao = document.getElementById('btn-dark-mode');
     if (document.body.classList.contains('dark-mode')) {
         botao.textContent = '☀️ Modo Claro';
